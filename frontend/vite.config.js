21c43f52a0f4db216ad0d5fdb11d827c6285657e
Rahul Saliya <RHS.Dev@outlook.com>
2023-11-28 12:15:51 -0400

    - updated vite.config.js.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,21 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    host: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3001'
       }
     }
-  }
+  },
+  preview: {
+    port: 3000,
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001'
+      }
+    }
+  },
+
 })
