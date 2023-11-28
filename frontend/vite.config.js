628a47b82880c780ee9f290a5f2d68bed879fc9f
Rahul Saliya <RHS.Dev@outlook.com>
2023-11-28 12:30:56 -0400

    - added support for env variables.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    host: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001'
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: 3000,
+      host: true,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3001'
+        }
       }
-    }
-  },
-  preview: {
-    port: 3000,
-    host: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001'
+    },
+    preview: {
+      port: 3000,
+      host: true,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3001'
+        }
       }
-    }
-  },
+    },
 
-})
+  })
+};
