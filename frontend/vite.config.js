fca4b09afc8c2169667ac32fb521a29afebf4c86
Rahul Saliya <RHS.Dev@outlook.com>
2023-11-29 21:18:56 -0400

    - added db target env variable.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,9 @@ export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return defineConfig({
     plugins: [react()],
+    optimizeDeps: {
+      exclude: ['js-big-decimal']
+    },
     server: {
       port: 3000,
       host: true,
